7512820849269f696ae1aa6b9757c18d9aa819a7
Karl Jeppesen <32724999+KarlJeppesen@users.noreply.github.com>
2019-05-11 21:15:42 +0200

    Brugervalidering i main fungerer

--- Gruppe2_projekt/src/main/Main.java
+++ Gruppe2_projekt/src/main/Main.java
@@ -8,33 +8,98 @@ public class Main {
 
     public static void main (String [ ] args) throws DALException{
 
+        System.out.println("Ønsker du at agere som\nAdministrator tryk 1\nLaborant tryk 2\nPharmaceut tryk 3\nProduktionsleder tryk 4");
+
         Scanner scan = new Scanner(System.in);
 
-        System.out.print("Indtast brugerID");
+        boolean valid = false;
 
-        String brugerID = scan.nextLine();
+        int rollevalg = scan.nextInt();
 
-        validateUser(brugerID);
+        System.out.print("Indtast brugerID\n");
 
-        //System.out.println("Ønsker du at agere som admin tryk 1, lab tryk 2, farma tryk 3 og leder tryk 4");
+        Scanner scan1 = new Scanner(System.in);
+
+        String brugerID = scan1.nextLine();
+
+        System.out.printf(brugerID);
 
         //TODO: Tjek at brugeren har rettigheder til at agere således
 
+        switch(rollevalg) {
+            case 1:
+                // code block
+                System.out.println("Du har valgt administrator");
+                validateUser("Administrators", brugerID);
+                break;
+            case 2:
+                // code block
+                System.out.println("Du har valgt laborant");
+                validateUser("Laborants", brugerID);
+                break;
+            case 3:
+                // code block
+                System.out.println("Du har valgt pharmaceut");
+                validateUser("Pharmaceuts", brugerID);
+                break;
+            case 4:
+                // code block
+                System.out.println("Du har valgt produktionsleder");
+                validateUser("Produktionsledere", brugerID);
+                break;
+            default:
+                // code block
+                System.out.println("Det indtastede var ikke et af de fire valg.");
+                main(args);
+        }
+
         //TODO: Hvis godkendt, sæt bruger som tilsvarende DAO.
+
+        if (valid) {
+
+            switch(rollevalg) {
+                case 1:
+                    // code block
+                    System.out.printf("Valg for admin");
+                    break;
+                case 2:
+                    // code block
+                    System.out.printf("Valg for laborant");
+                    break;
+                case 3:
+                    // code block
+                    System.out.printf("Valg for pharmaceut");
+                    break;
+                case 4:
+                    // code block
+                    System.out.printf("Valg for produktionsleder");
+                    break;
+                default:
+                    // code block
+            }
+
+        }
 
         //TODO: Implementer exit funktion.
 
     }
 
-    private static boolean validateUser(String brugerID) throws DALException {
+    private static boolean validateUser(String tabel, String brugerID) throws DALException {
 
         boolean validated = false;
 
         try (Connection c = DataSource.getConnection()) {
 
             Statement statement = c.createStatement();
-            ResultSet resultSet = statement.executeQuery("SELECT BrugerID FROM Bruger WHERE BrugerID = " + brugerID);
-            if (resultSet.toString() == brugerID) {
+            ResultSet resultSet = statement.executeQuery("SELECT BrugerID FROM "+tabel+" WHERE BrugerID = " + brugerID);
+
+            String output = "";
+
+            if (resultSet.next()) {
+                output = resultSet.getString("BrugerID");
+            }
+
+            if (output.equals(brugerID)) {
                 validated = true;
                 System.out.println("Brugeren er valideret.");
             } else {
